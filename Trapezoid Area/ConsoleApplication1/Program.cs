df8d25b30aff0f15cdd932b0f1b648adaed184aa
Frozaskes <seisuki@abv.bg>
2017-01-19 19:48:25 +0000

    Trapezoid Area

---
+++ Trapezoid Area/ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace Trapezoid_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                var a = double.Parse(Console.ReadLine());
+                var h = double.Parse(Console.ReadLine());
+
+
+                var area = a * h / 2;
+                area = Math.Round(area, 2);
+
+                Console.WriteLine("Area = " + area);
+                ///
+                {
+                    Console.WriteLine((pricefru * kgveg) * 1.94);
+                }
+            else if (pricefru <= 0 && pricefru >= 1000)
+                {
+                    Console.WriteLine((pricefru * kgfru) * 1.95);
+                }
+            }
+        }
+
+    }
+
+
+
