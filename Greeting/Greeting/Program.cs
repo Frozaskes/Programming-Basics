cf14db3aa5aa6ad38a8d646cf77b801def78ecc9
Frozaskes <seisuki@abv.bg>
2017-01-19 20:21:11 +0000

    Greeeting

---
+++ Greeting/Greeting/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter you name:" );
+             string name = Console.ReadLine();
+            Console.WriteLine("Hello, {0}!", name);
+
+            if (name == "SoftUni")
+            {
+                Console.WriteLine("Soft Uni University");
+            }
+            else
+            {
+                
+            }
+        }
+    }
+}
